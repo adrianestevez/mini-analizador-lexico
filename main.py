872e78592a24d73ae832c7ebe632e2c7f0ef2020
Adrián Estévez <Adrian.Estevez@alumno.udg.mx>
2021-08-24 21:17:07 -0500

    Aniadiendo captura ejecucion y diagramas de flujo

--- main.py
+++ main.py
@@ -1,44 +1,28 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from lexico import Lexico
 
-
-def main():
-
-    entradas_validas_real = ['2','2.666','2.5895', '11999','988']
-
-    entradas_validas_ident = ['adrian','_adrian','adrian_25', 'variable_nueva','variable123']
-
-    entradas_invalidas_real = ['.2', '6.', '3e', '3ad', '22.65ff']
-
-    entradas_invalidas_ident = ['3adrian',' adrian','$', '(&$#%/)','123']
-
-    reales = []
-    identificadores = []
+def procesar(array):
 
     print("Entrada\t\tTipo")
-    for entrada in entradas_validas_real:
-        lex = Lexico(entrada)
-        result = lex.definir()
-        print( entrada + "\t\t" + result)
-
-    print("Entrada\t\tTipo")
-    for entrada in entradas_validas_ident:
+    for entrada in array:
         lex = Lexico(entrada)
         result = lex.definir()
         print(entrada + "\t\t" + result)
 
-    print("Entrada\t\tTipo")
-    for entrada in entradas_invalidas_real:
-        lex = Lexico(entrada)
-        result = lex.definir()
-        print(entrada + "\t\t" + str(result))
+    print()
 
-    print("Entrada\t\tTipo")
-    for entrada in entradas_invalidas_ident:
-        lex = Lexico(entrada)
-        result = lex.definir()
-        print(entrada + "\t\t" + str(result))
 
+def main():
+
+    entradas_validas_real = ['2','2.666','2.5895', '11999','988'] #arreglo de entradas validas reales
+    entradas_validas_ident = ['adrian','_adrian','adrian_25', 'variable_nueva','variable123']#arreglo de entradas validas identificador
+    entradas_invalidas_real = ['.2', '6.', '3e', '3ad', '22.65ff']#arreglo de entradas NO validas reales
+    entradas_invalidas_ident = ['3adrian',' adrian','$', '(&$#%/)','123']#arreglo de entradas NO validas identificador
+
+    procesar(entradas_validas_real)
+    procesar(entradas_validas_ident)
+    procesar(entradas_invalidas_real)
+    procesar(entradas_invalidas_ident)
 
 
 main()
